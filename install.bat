a130ed3b7885226b05f2257e805f0a8b6e1315d2
Coen van den Munckhof <c.munckhof@gmail.com>
2020-08-05 23:19:12 +0200

    Targeting Core 3.1

--- install.bat
+++ install.bat
@@ -9,4 +9,8 @@ move .\download\LibGit2Sharp.NativeBinaries.2.0.298\runtimes\win-x64\native\*.dl
 del .\src\PdfGenerator\costura32\*.dll
 move .\download\LibGit2Sharp.NativeBinaries.2.0.298\runtimes\win-x86\native\*.dll .\src\PdfGenerator\costura32\
 
+
+nuget.exe install Microsoft.Office.Interop.Word -Version 15.0.4797.1003 -o .\download
+move .\download\Microsoft.Office.Interop.Word.15.0.4797.1003\lib\net20\Microsoft.Office.Interop.Word.dll .\src\
+
 del /S /Q .\download
