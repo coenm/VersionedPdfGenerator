d548a1e9e94a094b25a9509771d1368e409a19c1
Coen van den Munckhof <c.munckhof@gmail.com>
2020-09-01 13:45:51 +0200

    QR code generation for email; added qr code test using Verify.ImageSharp;

--- src/WebHost/Controllers/QrController.cs
+++ src/WebHost/Controllers/QrController.cs
@@ -15,25 +15,37 @@
         [HttpGet("text/{message}")]
         public IActionResult Text(string message)
         {
-            var payload = HttpUtility.UrlDecode(message);
+            var payload = Decode(message);
             return CreateQrImage(payload);
         }
 
         [HttpGet("url/{url}")]
         public IActionResult QrUlr(string url)
         {
-            var generator = new PayloadGenerator.Url(HttpUtility.UrlDecode(url));
+            var generator = new PayloadGenerator.Url(Decode(url));
             var payload = generator.ToString();
             return CreateQrImage(payload);
         }
 
+        [HttpGet("mail/{to}/{subject}/{message}")]
+        public IActionResult QrUlr(string to, string subject, string message)
+        {
+            var generator = new PayloadGenerator.Mail(Decode(to), Decode(subject), Decode(message));
+            var payload = generator.ToString();
+            return CreateQrImage(payload);
+        }
+
+        private static string Decode(string input)
+        {
+            return HttpUtility.UrlDecode(input);
+        }
+
         private IActionResult CreateQrImage(string payload)
         {
-            var qrGenerator = new QRCodeGenerator();
-            var qrCodeData = qrGenerator.CreateQrCode(payload, QRCodeGenerator.ECCLevel.Q);
-            var qrCode = new QRCode(qrCodeData);
-            var qrCodeImage = qrCode.GetGraphic(20);
-
+            using var qrGenerator = new QRCodeGenerator();
+            using var qrCodeData = qrGenerator.CreateQrCode(payload, QRCodeGenerator.ECCLevel.Q);
+            using var qrCode = new QRCode(qrCodeData);
+            using var qrCodeImage = qrCode.GetGraphic(20);
             using var ms = new MemoryStream();
             qrCodeImage.Save(ms, ImageFormat.Png);
             return File(ms.ToArray(), "image/png");
--- tests/WebHost.Tests/IntegrationWebHostQrCode.cs
+++ tests/WebHost.Tests/IntegrationWebHostQrCode.cs
@@ -1,5 +1,6 @@
 ﻿namespace WebHost.Tests
 {
+    using System.Net.Http;
     using System.Threading.Tasks;
     using System.Web;
 
@@ -7,12 +8,23 @@
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.TestHost;
     using Microsoft.Extensions.Hosting;
+    using SixLabors.ImageSharp;
+    using VerifyTests;
+    using VerifyXunit;
     using Xunit;
+    using Xunit.Categories;
 
+    [UsesVerify]
     public class IntegrationWebHostQrCode : IAsyncLifetime
     {
         private readonly IHostBuilder _hostBuilder;
+        private readonly VerifySettings _settings;
         private IHost _host;
+
+        static IntegrationWebHostQrCode()
+        {
+            VerifyImageSharp.Initialize();
+        }
 
         public IntegrationWebHostQrCode()
         {
@@ -23,40 +35,57 @@
                                       webHost.UseTestServer();
                                       webHost.UseStartup<Startup>();
                                   });
+
+            _settings = new VerifySettings();
+            _settings.UseExtension("png");
         }
 
         [Fact]
+        [Category("ImageVerify")]
         public async Task QrUrlShouldReturnImageWithQrCode()
         {
             // arrange
-            var client = _host.GetTestClient();
+            using var client = _host.GetTestClient();
 
             // act
             var response = await client.GetAsync($"/qr/url/{EncodeUrl("www.google.com")}");
-            var bytes = await response.Content.ReadAsByteArrayAsync();
 
             // assert
             response.IsSuccessStatusCode.Should().BeTrue();
-            bytes.Length.Should().Be(21013);
-
-            // should be better to verify image.
+            await AssertQrCodeImage(response);
         }
 
         [Fact]
+        [Category("ImageVerify")]
         public async Task TextShouldReturnImageWithQrCode()
         {
             // arrange
-            var client = _host.GetTestClient();
+            using var client = _host.GetTestClient();
 
             // act
             var response = await client.GetAsync($"/qr/text/{EncodeUrl("www.google.com")}");
-            var bytes = await response.Content.ReadAsByteArrayAsync();
 
             // assert
             response.IsSuccessStatusCode.Should().BeTrue();
-            bytes.Length.Should().Be(21013);
+            await AssertQrCodeImage(response);
+        }
 
-            // should be better to verify image.
+        [Fact]
+        [Category("ImageVerify")]
+        public async Task MailShouldReturnImageWithQrCode()
+        {
+            // arrange
+            const string MESSAGE = @"Here is some feedback
+Line 2
+Line 3";
+            using var client = _host.GetTestClient();
+
+            // act
+            var response = await client.GetAsync($"/qr/mail/{EncodeUrl("me@h0st.com")}/{EncodeUrl("Feedback on version abc")}/{EncodeUrl(MESSAGE)}");
+
+            // assert
+            response.IsSuccessStatusCode.Should().BeTrue();
+            await AssertQrCodeImage(response);
         }
 
         public async Task InitializeAsync()
@@ -73,6 +102,13 @@
         private static string EncodeUrl(string url)
         {
             return HttpUtility.UrlEncode(url);
+        }
+
+        private async Task AssertQrCodeImage(HttpResponseMessage response)
+        {
+            var bytes = await response.Content.ReadAsByteArrayAsync();
+            using var img = Image.Load(bytes);
+            await Verifier.Verify(img, _settings);
         }
     }
 }
